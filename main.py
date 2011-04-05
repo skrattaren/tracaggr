8fb47d99b19a476c0aec8284ec941a9f26dc00c2
Nickolaj Stjujsckij <sterkrig@home.no>
2011-04-05 13:32:24 +0400

    Implement basic PgSQL data fetching

--- main.py
+++ main.py
@@ -2,12 +2,41 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
+
 from flask import Flask, render_template
 app = Flask('TracAggr')
 
+TRAC_DATA = (
+    {'name': 'Name_1', 'host': 'host1', 'port': 5432,
+     'database': 'TracDB', 'user': 'tracuser'},
+)
+
+try:
+    from settings import TRAC_DATA
+except ImportError:
+    print("Settings not found")
+
+QUERY = "SELECT id, owner FROM ticket WHERE status!='closed'"
+
+# prepare dictionary of DB connections
+TRACS = []
+for trac in TRAC_DATA:
+    TRACS.append({'name': trac.pop('name'), 'conn': psycopg2.connect(**trac)})
+    TRACS[-1]['conn'].set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    result = {}
+    for trac in TRACS:
+        cur = trac['conn'].cursor()
+        cur.execute(QUERY)
+        result[trac['name']] = cur.fetchall()
+    return render_template('index.html', data=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
