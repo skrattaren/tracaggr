5bb7230dc0de49d4ffc63f81bf8d1d955a8ad0df
Nickolaj Stjujsckij <sterkrig@home.no>
2011-04-06 18:23:17 +0400

    Display ticket link and summary

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import calendar
 import datetime
 
 import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT, UNICODE, UNICODEARRAY
 from psycopg2.extras import DictCursor
 
-psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
-psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
+psycopg2.extensions.register_type(UNICODE)
+psycopg2.extensions.register_type(UNICODEARRAY)
 
 from flask import Flask, render_template
 app = Flask('TracAggr')
@@ -19,7 +19,8 @@ from utils import dictify
 
 # Trac databases
 TRAC_DATA = (
-    {'name': 'Name_1', 'host': 'host1', 'port': 5432,
+    {'name': 'Name_1', 'base_url': 'http://example.com/trac',
+     'host': 'host1', 'port': 5432,
      'database': 'TracDB', 'user': 'tracuser'},
 )
 
@@ -37,7 +38,8 @@ except ImportError:
 # query to fetch open tickets
 QUERY_OPEN = "SELECT id, owner FROM ticket WHERE status!='closed'"
 # query to fetch month data
-QUERY_MONTH = """SELECT ticket.id, ticket.owner, ticket_custom.value AS due_date
+QUERY_MONTH = """SELECT ticket.id, ticket.owner, ticket.summary,
+                        ticket_custom.value AS due_date
                 FROM ticket INNER JOIN ticket_custom
                 ON ticket.id = ticket_custom.ticket
                 WHERE ticket_custom.name='due_date' AND
@@ -48,8 +50,10 @@ QUERY_CLSD_WEEK = "SELECT id, owner FROM ticket WHERE status='closed'"
 # prepare dictionary of DB connections
 TRACS = []
 for trac in TRAC_DATA:
-    TRACS.append({'name': trac.pop('name'), 'conn': psycopg2.connect(**trac)})
+    TRACS.append({'name': trac.pop('name'), 'base_url': trac.pop('base_url'),
+                  'conn': psycopg2.connect(**trac)})
     TRACS[-1]['conn'].set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    TRACS[-1]['conn'].set_client_encoding('utf-8')
 
 # sort developer list
 DEVS.sort(cmp=lambda x,y: cmp(x[0], y[0]))
@@ -68,7 +72,8 @@ def index():
         cur.execute(QUERY_MONTH, (monthstr, ))
         month_data_raw = dictify(cur.fetchall(), 'owner',
                              dict2up=month_data_raw,
-                             add_data={'trac': trac['name']})
+                             add_data={'trac': trac['name'],
+                                       'base_url': trac['base_url']})
     for user, tickets in month_data_raw.items():
         month_data[user] = dictify(tickets, 'due_date')
     # create calendars
